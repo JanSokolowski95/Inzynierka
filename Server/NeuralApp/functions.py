98c10ed06534225540dddca65f61c79c16eaf13c
Kacper Skoczek <skoczekk96@gmail.com>
2018-10-28 22:07:24 +0100

    Add neural network

--- Server/NeuralApp/functions.py
+++ Server/NeuralApp/functions.py
@@ -1,12 +1,14 @@
 from PIL import Image
 import numpy as np
-#from .predict import Network
+from .predict import Network
 
 def handle_uploaded_file(image):
     img = Image.open(image)
     img = img.resize((224, 224), Image.ANTIALIAS)
+    
+    network = Network('./NeuralApp/weights.h5')
 
-    #result = Network.predict(img)
+    result = network.predict(img)
     
     #img.show()
-    return ['it', 'just', ' works']
+    return result
---
+++ Server/NeuralApp/predict.py
@@ -0,0 +1,31 @@
+import numpy as np
+from keras.preprocessing import image
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+
+# dimensions of our images.
+(img_width, img_height) = (224, 224)
+
+class Network:
+    def __init__(self, weights_path):
+        self.weights_path = weights_path
+
+        self.model = VGG16(include_top=False, weights='imagenet')
+
+        self.top_model = Sequential()
+        self.top_model.add(Flatten(input_shape=(7, 7, 512)))
+        self.top_model.add(Dense(256, activation='relu'))
+        self.top_model.add(Dropout(0.5))
+        self.top_model.add(Dense(10, activation='softmax'))
+        self.top_model.load_weights(weights_path)
+    
+    def predict(self, img):
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        
+        features = self.model.predict(x)
+        img_class = self.top_model.predict(features)
+        return img_class[0]
