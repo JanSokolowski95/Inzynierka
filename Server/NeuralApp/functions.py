46216a8e0cf4ff6dcf6c0e97080041bd50c4c80c
jasiek <Promocja95@gmail.com>
2018-10-28 21:35:28 +0100

    Change 'results.html' template and 'functions.py' file

--- Server/NeuralApp/functions.py
+++ Server/NeuralApp/functions.py
@@ -1,6 +1,12 @@
 from PIL import Image
+import numpy as np
+#from .predict import Network
 
 def handle_uploaded_file(image):
-    #img = Image.open(image)
+    img = Image.open(image)
+    img = img.resize((224, 224), Image.ANTIALIAS)
+
+    #result = Network.predict(img)
+    
     #img.show()
-    return 'it works'
+    return ['it', 'just', ' works']
