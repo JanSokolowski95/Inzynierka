d44bc302b348908dcbc3b757c1e914893d5fad47
Kacper Skoczek <skoczekk96@gmail.com>
2018-11-12 17:43:12 +0100

    Fixed weights.h5 path

--- Server/NeuralApp/functions.py
+++ Server/NeuralApp/functions.py
@@ -1,12 +1,13 @@
 from PIL import Image
 import numpy as np
 from .predict import Network
+from django.conf import settings
 
 def handle_uploaded_file(image):
     img = Image.open(image)
     img = img.resize((224, 224), Image.ANTIALIAS)
     
-    network = Network('./NeuralApp/weights.h5')
+    network = Network(settings.WEIGHTS_PATH)
 
     result = network.predict(img)
     
