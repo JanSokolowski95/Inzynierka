f041c0a9e34a4d0e4a3dc2cc5cf630554688b5da
Kacper Skoczek <skoczekk96@gmail.com>
2018-11-16 22:34:15 +0100

    Fixed required field error

--- Server/NeuralApp/views.py
+++ Server/NeuralApp/views.py
@@ -4,17 +4,16 @@ from .forms import UploadImageForm
 from .functions import handle_uploaded_file
 
 def upload_file(request):
-    form = 'Dummy String'
-    form = UploadImageForm(data = request.POST, files = request.FILES)
+    
     if request.method == 'POST':
+        form = UploadImageForm(data = request.POST, files = request.FILES)
         print(form.errors)
         if form.is_valid():
             print('succes uploading file')
             results = handle_uploaded_file(request.FILES['image'])
             return render(request, 'NeuralApp/results.html', {'results': results})
-        else:
-            print('error uploading file')
-            form = UploadImageForm()
+    else:
+        form = UploadImageForm()
     return render(request, 'NeuralApp/home.html', {'form': form})
 
 def results(request):
